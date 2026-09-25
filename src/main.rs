a20e337c8fb3e98fdd761aa71528fc695f23470a
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2026-09-25 21:57:37 +0200

    PWM Pin15 example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-//! This example test the RP Pico on board LED.
-//!
-//! It does not work with the RP Pico W board. See `blinky_wifi.rs`.
+//! PWM example for the RP Pico2.
 
 #![no_std]
 #![no_main]
@@ -8,9 +6,10 @@
 use defmt::*;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_rp::gpio;
+use embassy_rp::Peri;
+use embassy_rp::peripherals::{PIN_15, PWM_SLICE7};
+use embassy_rp::pwm::{Config, Pwm, SetDutyCycle};
 use embassy_time::Timer;
-use gpio::{Level, Output};
 use panic_probe as _;
 
 // Program metadata for `picotool info`.
@@ -18,26 +17,47 @@ use panic_probe as _;
 #[unsafe(link_section = ".bi_entries")]
 #[used]
 pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
-    embassy_rp::binary_info::rp_program_name!(c"Blinky Example"),
+    embassy_rp::binary_info::rp_program_name!(c"PWM Example"),
     embassy_rp::binary_info::rp_program_description!(
-        c"This example tests the RP Pico on board LED, connected to gpio 25"
+        c"This example tests the RP Pico2 PWM output on gpio 15"
     ),
     embassy_rp::binary_info::rp_cargo_version!(),
     embassy_rp::binary_info::rp_program_build_attribute!(),
 ];
 
 #[embassy_executor::main(executor = "embassy_rp::executor::Executor", entry = "cortex_m_rt::entry")]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_25, Level::Low);
+    spawner.spawn(pwm(p.PWM_SLICE7, p.PIN_15).unwrap());
+}
+
+#[embassy_executor::task]
+async fn pwm(slice7: Peri<'static, PWM_SLICE7>, pin15: Peri<'static, PIN_15>) {
+    let desired_freq_hz: u32 = 25_000;
+    let clock_freq_hz = embassy_rp::clocks::clk_sys_freq();
+    let divider = 16u8;
+
+    // Panic if divider is not large enough to make top fit into u16,
+    // or if the desired frequency is too high for the given clock and divider.
+    let top: u16 = (clock_freq_hz / (desired_freq_hz * divider as u32))
+        .checked_sub(1)
+        .and_then(|t| t.try_into().ok())
+        .expect("PWM frequency out of range for this divider");
+
+    let mut c = Config::default();
+    c.top = top;
+    c.divider = divider.into();
+
+    debug!("PWM configured with top: {}", top);
+    debug!("PWM configured with divider: {}", divider);
+
+    let mut pwm = Pwm::new_output_b(slice7, pin15, c);
 
     loop {
-        info!("led on!");
-        led.set_high();
-        Timer::after_millis(500).await;
-
-        info!("led off!");
-        led.set_low();
-        Timer::after_millis(500).await;
+        pwm.set_duty_cycle(0).unwrap();
+        Timer::after_secs(1).await;
+        pwm.set_duty_cycle_percent(25).unwrap();
+        Timer::after_secs(1).await;
     }
+
 }
